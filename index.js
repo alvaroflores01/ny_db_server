b96e7edfd3a465d8edd83f1716adf7d6db10c347
alvaroflores <alvaro@brolty.com>
2024-02-29 13:05:19 -0500

    added express boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// test('should first', () => { second })
+const express = require('express');
+const app = express();
+const port = 4000;
+
+
+// API ENDPOINTS
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+//LISTENING ON PORT
+app.listen(port, ()=> {
+    console.log(`Listening on ${port}`)
+})
